965f3eacd19456db4300e4a6efc45a9d7b4610fa
Eugene-fill <123318728+Eugene-fill@users.noreply.github.com>
2023-04-30 22:25:12 +0300

    Add files via upload

---
+++ lesson_20/js/script.js
@@ -0,0 +1,20 @@
+"use strict"
+
+
+
+
+let varOne = 10;
+let varTwo = 20;
+
+
+
+if(varOne > varTwo){
+    let result = varOne + varTwo;
+    console.log(result);
+}else if (varOne == 10) {
+    console.log("varOne = 10");
+} else {
+    console.log('else');
+}
+
+
